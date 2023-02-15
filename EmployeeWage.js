b540d824d02558de7eff5d51146a4c676a4f1066
Sourav2707 <smartsourav33@gmail.com>
2023-02-15 18:54:57 +0530

    UC1 Ability to Check Employee is present or Absent - Use Math.Random to

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,9 @@
 console.log("Welcome to day 38 employee wage program using javascript");
+const is_Absent = 0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck == is_Absent) {
+    console.log("Employee is Absent");
+}
+else {
+    console.log("Employee is Present");
+}
