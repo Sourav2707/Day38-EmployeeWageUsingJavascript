80e3039e884262c1e931ee6b7b08b6264a3ebf46
Sourav2707 <smartsourav33@gmail.com>
2023-02-16 17:14:19 +0530

    UC4 Calculating Wages for a Month assuming 20 Working Days in a Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,23 +6,31 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 let empHrs = 0;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalEmpWage = 0;
 function wageCalculation() {
+for(let i = 0; i < NUM_OF_WORKING_DAYS; i++) {
     let empCheck = Math.floor(Math.random()*10)%3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        console.log("Part time employee");
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        console.log("Full time employee");
-        empHrs = FULL_TIME_HOURS;
-        break;
-    case IS_ABSENT:
-        console.log("Employee is absent");
-        empHrs = 0;
-        break;        
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage : "+empWage);
+    switch (empCheck) {
+        case IS_PART_TIME:
+            console.log("Part time employee");
+            empHrs = PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            console.log("Full time employee");
+            empHrs = FULL_TIME_HOURS;
+            break;
+        case IS_ABSENT:
+            console.log("Employee is absent");
+            empHrs = 0;
+            break;        
+    }
+    let empWage = empHrs * WAGE_PER_HOUR;
+    totalEmpHrs += empHrs;
+    totalEmpWage = totalEmpHrs * WAGE_PER_HOUR; 
+    console.log("Employee Wage for the day "+(i+1)+" : "+empWage);
+    }
+    console.log("Total working hour for the month is "+totalEmpHrs+" and total wage is "+totalEmpWage);
 }
 wageCalculation();
